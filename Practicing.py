9104703587b08738a79d5ac9b38b5f0affc02ea6
jaironberiguete <jaironberiguete@gmail.com>
2025-02-05 11:08:13 -0500

    Add files via upload

---
+++ Practicing.py
@@ -0,0 +1,12 @@
+
+def suma (num1,num2):
+    total = num1 + num2
+    print(total)
+
+suma(23,40)
+newNumber = sum
+
+if (newNumber > 10 ):
+    print("The number is greater than 10")
+else:
+    print("Otherwise")
